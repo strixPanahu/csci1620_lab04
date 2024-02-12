b0f00034424d1fdd775e19cc6a9cba808bc73db5
strixpanahu <schimpfzach@gmail.com>
2024-02-12 14:24:52 -0600

    Completing steps 3, 4, 5, 6, 7, & 8-a

--- ZacSchimpf4.py
+++ ZacSchimpf4.py
@@ -5,12 +5,47 @@
     Week 04 - Lab 04
     12/02/2024
 """
+import csv
+import os
 import sys
 
 
-def main(raw_input):
-    pass
+def main():
+    raw_input = read_txt()
+
+    emails_dict = convert_raw_to_dict(raw_input)
+
+    output_to_csv(emails_dict)
+
+
+def read_txt():
+    inbound_name = "input.txt"
+    try:
+        with open(inbound_name) as inbound_file:
+            lines = inbound_file.readlines()
+        inbound_file.close()
+    except FileNotFoundError:
+        sys.exit("Invalid request for file_name \"" + inbound_name + "\" at \"" + os.getcwd() + "\"")
+
+    return lines
+
+
+def convert_raw_to_dict(raw_input):
+    return [{"Email": "", "Day": "", "Date": "", "Month": "", "Year": "", "Time":""}]
+
+
+def output_to_csv(emails_dict):
+    outbound_name = "outbound.csv"
+    header = [f"Email", f"Day", f"Date", f"Month", f"Year", f"Time"]
+    with open(outbound_name, 'w', newline='') as outbound_file:
+        writer = csv.DictWriter(outbound_file, fieldnames=header, delimiter=',')
+        writer.writeheader()
+
+        for message in emails_dict:
+            writer.writerow(message)
+
+        outbound_file.close()
 
 
 if __name__ == '__main__':
-    main(sys.argv)
+    main()
